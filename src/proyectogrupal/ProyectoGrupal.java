f9f09f6e069ade49e5fe33195905c0995ec61b74
gersonyol <gersonyol@gmail.com>
2020-05-06 19:42:51 -0600

    Subiendo proyecto

---
+++ src/proyectogrupal/ProyectoGrupal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogrupal;
+
+/**
+ *
+ * @author Sergio
+ */
+public class ProyectoGrupal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
